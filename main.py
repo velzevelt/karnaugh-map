56d8c351c8596c07af8ea97a5699018a9c8095e0
velzevelt <novaraptor13@gmail.com>
2023-02-24 01:10:37 +0300

    upd

--- main.py
+++ main.py
@@ -22,6 +22,53 @@ def get_rand_function_result(var_qty: int) -> list:
     return np.random.randint(2, size=2**var_qty)
 
 
+DOC_START = """\\documentclass{article}
+
+\\usepackage[english, russian]{babel}
+\\usepackage[utf8]{inputenc}
+\\usepackage[T2A, T1]{fontenc}
+\\usepackage{amsmath}
+\\usepackage{amssymb}
+\\usepackage{dsfont}
+\\usepackage{setspace}
+\\usepackage{tipa}
+\\usepackage{relsize}
+\\usepackage{textcomp}
+\\usepackage{mathrsfs}
+\\usepackage{calligra}
+\\usepackage{wasysym}
+\\usepackage{ragged2e}
+\\usepackage{physics}
+\\usepackage{xcolor}
+\\usepackage{microtype}
+\\DisableLigatures{encoding = *, family = * }
+\\linespread{1}
+
+\\begin{document}
+
+
+\\begin{otherlanguage*}{russian}
+
+    
+\\end{otherlanguage*}
+
+
+\\end{document}
+
+"""
+
+
+"""
+\\begin{otherlanguage*}{russian}
+
+    В начале июля, в чрезвычайно жаркое время, под вечер, один молодой человек вышел из своей каморки, 
+    которую нанимал от жильцов в С — м переулке, на улицу и медленно, как бы в нерешимости, отправился к К — ну мосту
+    
+\\end{otherlanguage*}
+
+A word and another \\foreignlanguage{russian}{слово}
+"""
+
 # class IntegerTableExampe(Scene):
 #     def construct(self):
 #         var_qty = 2
@@ -41,10 +88,12 @@ def get_rand_function_result(var_qty: int) -> list:
 
 class FunctionNormalForms(Scene):
     def construct(self):
-        title_1 = Title('Нормальные формы в логике')
+        # title_1 = Title('Нормальные формы в логике')
+        TexTemplate()
+        title_1 = Title(DOC_START, tex_environment='otherlanguage*')
         text_1 = 'Формула в булевой логике может быть записана в дизъюнктивной и в конъюнктивной нормальной форме. Также выделяют совершенную дизъюнктивную и совершенную конъюктивную нормальную форму'
-        tex_1 = Tex(f'\\justifying {text_1}')
+        # tex_1 = Tex(text_1)
 
         self.play(FadeIn(title_1))
-        self.play(FadeIn(tex_1))
+        # self.play(FadeIn(tex_1))
         self.wait()
