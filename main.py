974ad0abb05e24ca4c64a02c4f1374a0007e4325
velzevelt <novaraptor13@gmail.com>
2023-02-25 11:40:33 +0300

    upd

--- main.py
+++ main.py
@@ -197,6 +197,7 @@ class KarnaughMap(Scene):
         table_data = make_truth_table(var_qty)
         table_data = np.c_[table_data, function_result]
 
+        # Таблица истинности
         table = IntegerTable(table_data,
             include_outer_lines=True,
             top_left_entry=Tex('N'),
@@ -208,7 +209,6 @@ class KarnaughMap(Scene):
             line_config={'stroke_width': 1}
             )
         
-        # table.next_to(title_1, DOWN, buff=0.1)
 
         t_1 = Tex('$X_{2}$')
         t_2 = Tex('$X_{1}$')
@@ -221,7 +221,7 @@ class KarnaughMap(Scene):
         )
         top_left.rotate(-PI / 4)
 
-
+        # Карта координат
         karnaugh_map = MobjectTable(
             [[Tex(0), Tex(1)], [Tex(2), Tex(3)]], 
             include_outer_lines=True,
@@ -233,6 +233,7 @@ class KarnaughMap(Scene):
             line_config={'stroke_width': 1},
         )
 
+        # Настоящая карта
         karnaugh_map_2 = MobjectTable(
             [[Tex(0), Tex(0)], [Tex(1), Tex(1)]], 
             include_outer_lines=True,
@@ -247,25 +248,27 @@ class KarnaughMap(Scene):
         
         Group(table, karnaugh_map).arrange_in_grid(buff=1)
         
-        # Выделение координат
-        table.add(SurroundingRectangle(table.get_row_labels(), corner_radius=0.2))
         
-        needle = VGroup(karnaugh_map.get_columns()[1][1:], karnaugh_map.get_columns()[2][1:])
-        karnaugh_map.add(SurroundingRectangle(needle, corner_radius=0.2))
-        arrow_1 = Arrow(start=table.get_row_labels()[0], end=karnaugh_map.get_columns()[1][1])
 
         # Подстановка значений в координаты
-        table.add(SurroundingRectangle(table.get_columns()[-1][1:], corner_radius=0.2, color=GREEN))
-        arrow_2 = Arrow(start=table.get_columns()[-1][1], end=karnaugh_map.get_columns()[1][1])
+        # table.add(SurroundingRectangle(table.get_columns()[-1][1:], corner_radius=0.2, color=GREEN))
+        # arrow_2 = Arrow(start=table.get_columns()[-1][1], end=karnaugh_map.get_columns()[1][1])
 
-        self.add(arrow_1)
-        self.add(title_1)
-        self.add(table)
-        self.add(karnaugh_map)
+        # self.add(arrow_1)
+        # self.add(title_1)
+        # self.add(table)
+        # self.add(karnaugh_map)
         
-        # self.play(FadeIn(title_1))
-        # self.wait()
-        # self.play(table.create())
-        # self.wait()
-        # self.play(karnaugh_map.create())
-        # self.wait()
+        self.play(FadeIn(title_1))
+        self.wait()
+        self.play(table.create())
+        self.wait()
+        self.play(karnaugh_map.create())
+        self.wait()
+
+        # Выделение координат
+        self.play(table.animate.add(SurroundingRectangle(table.get_row_labels(), corner_radius=0.2)))
+        
+        # needle = VGroup(karnaugh_map.get_columns()[1][1:], karnaugh_map.get_columns()[2][1:])
+        # karnaugh_map.add(SurroundingRectangle(needle, corner_radius=0.2))
+        # arrow_1 = Arrow(start=table.get_row_labels()[0], end=karnaugh_map.get_columns()[1][1])
