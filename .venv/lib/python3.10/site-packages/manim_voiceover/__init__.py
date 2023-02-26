1c012b287ef710a716dfef971224ed5aa555d725
velzevelt <novaraptor13@gmail.com>
2023-02-26 15:53:08 +0300

    upd

--- .venv/lib/python3.10/site-packages/manim_voiceover/__init__.py
+++
@@ -1,6 +0,0 @@
-from manim_voiceover.tracker import VoiceoverTracker
-from manim_voiceover.voiceover_scene import VoiceoverScene
-
-import pkg_resources
-
-__version__: str = pkg_resources.get_distribution(__name__).version
